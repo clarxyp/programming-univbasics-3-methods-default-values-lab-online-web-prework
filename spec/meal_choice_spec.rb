bd961ebfdb699de06c63ceca8277f0de98057969
Clara Yip <clara.yip1@gmail.com>
2019-06-23 18:45:27 +0000

    Automatically backed up by Learn

--- spec/meal_choice_spec.rb
+++ spec/meal_choice_spec.rb
@@ -6,16 +6,25 @@ describe '#meal_choice' do
   end
 end
 
+<<<<<<< HEAD
 describe '#meal_choice_spec'do
 it 'should allow you to set a protein' do
+=======
+  it 'should allow you to set a protein' do
+>>>>>>> 0b9c767e2234427e375411f3133be8c6a30b977d
     expect(meal_choice("broccoli", "macaroni", "tofu")).to output("A plate of tofu with broccoli and macaroni.").to_stdout
   end
 end
 
 describe 
 
+<<<<<<< HEAD
 describe '#'
 it 'should puts "What a nutritious meal!" and your order to the console' do
+=======
+
+  it 'should puts "What a nutritious meal!" and your order to the console' do
+>>>>>>> 0b9c767e2234427e375411f3133be8c6a30b977d
     expect($stdout).to receive(:puts).to output("What a nutritious meal!").to_stdout
     expect($stdout).to receive(:puts).to output("A plate of meat with broccoli and macaroni.").to_stdout
     meal_choice("broccoli", "macaroni")
